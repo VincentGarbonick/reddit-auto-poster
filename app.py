ee98cd9093dd103cee7f5f215d127b2d8717301d
Vinnie <sheepking4444@gmail.com>
2025-06-25 01:10:27 -0400

    get auto stuff working

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import praw
 import os
+import time
+from datetime import datetime 
 from dotenv import load_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
 
 load_dotenv()
+
+def echo(text):
+    print(f"[{datetime.now()}] {text}")
 
 reddit = praw.Reddit(
     client_id=os.getenv('REDDIT_CLIENT_ID'),
@@ -14,3 +19,20 @@ reddit = praw.Reddit(
 )
 
 print(reddit)
+
+
+scheduler = BackgroundScheduler()
+
+# Define your jobs
+scheduler.add_job(echo, 'date', run_date=datetime(2025, 6, 25, 1, 8), args=["Job 1: 10:30"])
+scheduler.add_job(echo, 'date', run_date=datetime(2025, 6, 25, 1, 9), args=["Job 2: 11:00"])
+scheduler.add_job(echo, 'date', run_date=datetime(2025, 6, 25, 1, 10), args=["Job 3: 12:00"])
+
+scheduler.start()
+print("All jobs scheduled. Waiting...")
+
+try:
+    while True:
+        time.sleep(1)
+except (KeyboardInterrupt, SystemExit):
+    scheduler.shutdown()
