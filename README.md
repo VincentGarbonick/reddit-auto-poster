320742af7b59d575ec4930bb7ba9bb99fefd237f
Vinnie <sheepking4444@gmail.com>
2025-06-25 00:36:13 -0400

    update readme, get app working w stuff

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ schedules posts for reddit automatically, so you can get them at the perfect tim
 
 # Dependancies 
 1. praw
+2. dotenv (not needed per se, but super useful)
 
 # Jawn 
 Use this jawn to actually set up your app https://old.reddit.com/prefs/apps
--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-self.reddit = praw.Reddit(
+import praw
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+reddit = praw.Reddit(
     client_id=os.getenv('REDDIT_CLIENT_ID'),
     client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
     username=os.getenv('REDDIT_USERNAME'),
     password=os.getenv('REDDIT_PASSWORD'),
-    user_agent='AutoPostBot/1.0 by YourUsername'
+    user_agent=os.getenv('REDDIT_USER_AGENT')
 )
+
+print(reddit)
